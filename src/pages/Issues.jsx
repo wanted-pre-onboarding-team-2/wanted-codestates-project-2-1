454190fae493ce499ce2494dbc08b170c474eb47
mooroom <61869246+mooroom@users.noreply.github.com>
2022-02-22 22:58:18 +0900

    Update Issues.jsx

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,13 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { isS } from "xmlchars/xml/1.0/ed5";
 
 function Issues() {
   const { repoInfo } = useParams();
+  const [issues, setIssues] = useState(null);
+
   useEffect(() => {
-    console.log(repoInfo);
+    const [owner, repo] = repoInfo.split("-");
+    console.log(owner);
+    console.log(repo);
+
+    const fetchIssues = async () => {
+      const res = await axios.get(
+        `https://api.github.com/repos/${owner}/${repo}/issues`,
+      );
+      console.log(res.data);
+      setIssues(res.data);
+    };
+
+    fetchIssues();
   }, [repoInfo]);
 
-  return <div>Repo Info {repoInfo}</div>;
+  return (
+    <>
+      <div>[{repoInfo} ISSUES]</div>
+      {issues &&
+        issues.map((issue, idx) => (
+          <div key={idx}>
+            {idx + 1}. {issue.title}
+          </div>
+        ))}
+    </>
+  );
 }
 
 export default Issues;
