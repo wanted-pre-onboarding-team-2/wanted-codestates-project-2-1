153fc2f61f060493e9135c1614f9cca0985bb7d4
jinn2u <bsybear623@gmail.com>
2022-02-23 01:01:09 +0900

    Feat: RepoTitle에 lazy loading 추가

--- src/components/RepoTitle/index.jsx
+++ src/components/RepoTitle/index.jsx
@@ -1,21 +1,36 @@
+import { useEffect, useRef, useState } from "react";
+import { useInterSectionObserver } from "../../hooks/useInterSectionObserver";
 import { Wrapper, AddBtn, H1 } from "./style";
 
-const RepoTitle = ({ title, type, onBtnClick }) => {
+const RepoTitle = ({ title, type, onBtnClick, lazy, threshold }) => {
+  console.log(lazy);
+  const [loaded, setLoaded] = useState(false);
+  const titleRef = useRef(null);
+
   const bgColor = type === "add" ? "cornflowerblue" : "lightcoral";
   const btnContent = type === "add" ? "추가" : "삭제";
+  const isShow = !lazy ? true : lazy && loaded ? true : false;
+
+  let observer = null;
+
+  useInterSectionObserver(titleRef, setLoaded, threshold, lazy, observer);
 
   return (
-    <Wrapper>
-      <H1>{title}</H1>
-      <AddBtn
-        width={50}
-        height={30}
-        color="white"
-        bgColor={bgColor}
-        onBtnClick={onBtnClick}
-      >
-        {btnContent}
-      </AddBtn>
+    <Wrapper ref={titleRef}>
+      {isShow && (
+        <>
+          <H1>{title}</H1>
+          <AddBtn
+            width={50}
+            height={30}
+            color="white"
+            bgColor={bgColor}
+            onBtnClick={onBtnClick}
+          >
+            {btnContent}
+          </AddBtn>
+        </>
+      )}
     </Wrapper>
   );
 };
