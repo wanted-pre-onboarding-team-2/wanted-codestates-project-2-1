773862105918d634b9f1d495fb995ff9145d3991
hoyong <mooroom.dev@gmail.com>
2022-02-23 16:26:11 +0900

    Squashed commit of the following:
    commit 991cb0519c9c314016de2a975b328238d5aa0710
    Author: Leo-Xee <jang.xmin@gmail.com>
    Date:   Wed Feb 23 16:05:51 2022 +0900
    
        Feat: 페이지네이션 구현
    
    commit d02419fb1a4108d06b7d3364ae950e519e5c67c4
    Author: Leo-Xee <jang.xmin@gmail.com>
    Date:   Wed Feb 23 14:50:29 2022 +0900
    
        Refactor: IssueCard 파일분리

---
+++ src/components/IssueCard/IssueCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import * as S from "./style";
+
+function IssueCard({ issue, repoInfo }) {
+  const { html_url, title, user, created_at } = issue;
+
+  return (
+    <S.Card href={html_url} target="_blank">
+      <S.Title>{title}</S.Title>
+      <S.RepoName>{repoInfo}</S.RepoName>
+      <S.Avatar src={user.avatar_url} />
+      <S.Date>{created_at}</S.Date>
+    </S.Card>
+  );
+}
+
+export default IssueCard;
