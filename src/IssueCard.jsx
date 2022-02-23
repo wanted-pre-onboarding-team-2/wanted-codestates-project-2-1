874d84da3705e522177721acb3f6794eb9dbe4ab
Kae <kaehehehe@gmail.com>
2022-02-23 14:21:30 +0900

    Feat: Avatar, Data 추가

--- src/IssueCard.jsx
+++ src/IssueCard.jsx
@@ -6,26 +6,37 @@ const Card = styled.a`
   list-style: none;
   background-color: lightcoral;
   width: 400px;
-  height: 100px;
   margin: 10px;
   cursor: pointer;
 `;
 
 const Title = styled.h1`
-  font-size: 18px;
+  font-size: 1.8rem;
 `;
 
 const RepoName = styled.h3`
-  font-size: 15px;
+  font-size: 1.5rem;
+`;
+
+const Avatar = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+`;
+
+const Date = styled.span`
+  font-size: 1rem;
 `;
 
 function IssueCard({ issue, repoInfo }) {
-  const { html_url, title, img } = issue;
+  const { html_url, title, user, created_at } = issue;
 
   return (
-    <Card href={html_url}>
+    <Card href={html_url} target="_blank">
       <Title>{title}</Title>
       <RepoName>{repoInfo}</RepoName>
+      <Avatar src={user.avatar_url} />
+      <Date>{created_at}</Date>
     </Card>
   );
 }
