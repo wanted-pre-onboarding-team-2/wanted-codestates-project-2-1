991cb0519c9c314016de2a975b328238d5aa0710
Leo-Xee <jang.xmin@gmail.com>
2022-02-23 16:05:51 +0900

    Feat: 페이지네이션 구현

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
 
 import IssueCard from "../components/IssueCard";
 import styled from "styled-components";
@@ -11,23 +12,33 @@ const Cards = styled.div`
 
 function Issues() {
   const { repoInfo } = useParams();
+  const [owner, repo] = repoInfo.split("-");
+
   const [issues, setIssues] = useState(null);
 
-  useEffect(() => {
-    const [owner, repo] = repoInfo.split("-");
-    console.log(owner);
-    console.log(repo);
+  const fetchIssues = async currentPage => {
+    const res = await axios.get(
+      `https://api.github.com/repos/${owner}/${repo}/issues?page=${currentPage}`,
+    );
+    console.log(res.data);
+    return res.data;
+  };
 
-    const fetchIssues = async () => {
-      const res = await axios.get(
-        `https://api.github.com/repos/${owner}/${repo}/issues`,
-      );
-      console.log(res.data);
-      setIssues(res.data);
+  const handlePageClick = async data => {
+    let currentPage = data.selected + 1;
+    const issuesFromServer = await fetchIssues(currentPage);
+
+    setIssues(issuesFromServer);
+  };
+
+  useEffect(() => {
+    const initFetch = async () => {
+      const issuesFromServer = await fetchIssues(1);
+      setIssues(issuesFromServer);
     };
 
-    fetchIssues();
-  }, [repoInfo]);
+    initFetch();
+  }, []);
 
   return (
     <>
@@ -36,6 +47,24 @@ function Issues() {
         issues.map((issue, idx) => (
           <IssueCard key={idx + 1} issue={issue} repoInfo={repoInfo} />
         ))}
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        pageCount={10}
+        pageRangeDisplayed={6}
+        onPageChange={handlePageClick}
+        containerClassName={""}
+        pageClassName={""}
+        pageLinkClassName={""}
+        previousClassName={""}
+        previousLinkClassName={""}
+        nextClassName={""}
+        nextLinkClassName={""}
+        breakClassName={""}
+        breakLinkClassName={""}
+        activeClassName={""}
+      />
     </>
   );
 }
