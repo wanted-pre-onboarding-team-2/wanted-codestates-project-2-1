40cf353d7f58c2a6bbbfbb9dfa6d6fdcfdb02f5e
Kae <77221488+kaehehehe@users.noreply.github.com>
2022-04-01 23:29:41 +0900

    Fix: 로직 수정

--- src/util/convertDataIntoAgo.jsx
+++ src/util/convertDataIntoAgo.jsx
@@ -1,40 +1,29 @@
 export const convertDataIntoAgo = data => {
-  const [publishedYear, publishedMonth, publishedDay] = data
-    .slice(0, 10)
-    .split("-");
-  const [publishedHour, publishedMin, publishedSec] = data
-    .slice(11, 19)
-    .split(":");
   const today = new Date();
+  const timeValue = new Date(data);
 
-  if (today.getFullYear() !== Number(publishedYear)) {
-    const year = today.getFullYear() - publishedYear;
-    return year === 1 ? `${year} year` : `${year} years`;
+  const difference = Math.floor(
+    (today.getTime() - timeValue.getTime()) / 1000 / 60,
+  );
+
+  if (difference < 1) {
+    return "now";
   }
-  if (today.getMonth() + 1 !== Number(publishedMonth)) {
-    const month = today.getMonth() + 1 - publishedMonth;
-    return month === 1 ? `${month} month` : `${month} months`;
+
+  if (difference < 60) {
+    return difference === 1 ? `${difference} minute` : `${difference} minutes`;
   }
-  if (today.getDate() !== Number(publishedDay)) {
-    const day = today.getDate() - publishedDay;
-    if (day >= 7) {
-      const week = Math.floor(day / 7);
-      return week === 1 ? `${week} week` : `${week} weeks`;
-    } else {
-      return day === 1 ? `${day} day` : `${day} days`;
-    }
-  }
-  if (today.getHours() !== Number(publishedHour)) {
-    const hour = today.getHours() - Number(publishedHour);
+
+  const hour = Math.floor(difference / 60);
+  if (hour < 24) {
     return hour === 1 ? `${hour} hour` : `${hour} hours`;
   }
-  if (today.getMinutes() !== Number(publishedMin)) {
-    const min = today.getMinutes() - Number(publishedMin);
-    return min === 1 ? `${min} minute` : `${min} minutes`;
+
+  const day = Math.floor(difference / 60 / 24);
+  if (day < 365) {
+    return day === 1 ? `${day} day` : `${day} days`;
   }
-  if (today.getSeconds() !== Number(publishedSec)) {
-    const sec = today.getSeconds() - Number(publishedSec);
-    return sec === 1 ? `${sec} second` : `${sec} seconds`;
-  }
-  return "now";
+
+  const year = Math.floor(day / 365);
+  return year === 1 ? `${year} year` : `${year} years`;
 };
